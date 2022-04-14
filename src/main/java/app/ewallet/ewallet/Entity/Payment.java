bc189da7ec637de6bb0bc84d1cee7939fbd8d4e3
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-14 21:00:25 +0300

    E-Wallet

---
+++ src/main/java/app/ewallet/ewallet/Entity/Payment.java
@@ -0,0 +1,25 @@
+package app.ewallet.ewallet.Entity;
+
+import java.math.BigInteger;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Where;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "payment")
+@Data
+@Where(clause = "status = 'ACTIVE'")
+public class Payment extends Persistence{/**
+ *
+ */
+private static final long serialVersionUID = -4692510340887664687L;
+
+    private BigInteger creditWallet;
+
+    private BigInteger debitWallet;
+
+}
