65074e88d4fbc9c96aa0178cdcff67ac2c073dcd
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-14 21:00:41 +0300

    E-Wallet

--- src/main/java/app/ewallet/ewallet/Entity/Payment.java
+++ src/main/java/app/ewallet/ewallet/Entity/Payment.java
@@ -13,9 +13,8 @@ import lombok.Data;
 @Table(name = "payment")
 @Data
 @Where(clause = "status = 'ACTIVE'")
-public class Payment extends Persistence{/**
- *
- */
+public class Payment extends Persistence{
+
 private static final long serialVersionUID = -4692510340887664687L;
 
     private BigInteger creditWallet;
