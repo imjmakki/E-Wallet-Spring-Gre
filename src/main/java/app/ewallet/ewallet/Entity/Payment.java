f8307310751b7b053b15d430f14088742b3a82df
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-14 21:00:55 +0300

    E-Wallet

--- src/main/java/app/ewallet/ewallet/Entity/Payment.java
+++ src/main/java/app/ewallet/ewallet/Entity/Payment.java
@@ -18,7 +18,6 @@ public class Payment extends Persistence{
 private static final long serialVersionUID = -4692510340887664687L;
 
     private BigInteger creditWallet;
-
     private BigInteger debitWallet;
 
 }
